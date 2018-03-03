c6588857a5fd91e57a5cb0e189508b76dd1e2784
Vu Tran <vutran@dropbox.com>
2018-03-03 13:48:19 -0800

    add readme; build script; file menus

---
+++ README.md
@@ -0,0 +1,7 @@
+<p align="center">
+    <h1>Vzl</h1>
+</p>
+
+## Special Thanks
+
+- [mdaine](https://github.com/mdaines/) for providing the wonderful [`viz.js`](https://github.com/mdaines/viz.js) library
--- app/main/index.js
+++ app/main/index.js
@@ -1,22 +1,32 @@
+const fs = require('fs');
 const path = require('path');
 const url = require('url');
 const Viz = require('viz.js');
-const { app, BrowserWindow, ipcMain } = require('electron');
+const pkg = require('../../package.json');
+const {
+    app,
+    BrowserWindow,
+    dialog,
+    ipcMain,
+    Menu,
+    shell,
+} = require('electron');
+
+let svg = null;
 
 let mainWindow = null;
 
 ipcMain.on('editor', (evt, msg) => {
     try {
-        const svg = Viz(msg);
+        svg = Viz(msg);
         evt.sender.send('preview', svg);
-    } catch (err) { console.error(err); }
+    } catch (err) {}
 });
 
 function createWindow() {
     mainWindow = new BrowserWindow({
         titleBarStyle: 'hiddenInset',
         textAreasAreResizable: false,
-
     });
 
     mainWindow.on('closed', () => {
@@ -25,11 +35,68 @@ function createWindow() {
 
     const pathname = path.resolve(__dirname, '..', 'renderer', 'index.html');
 
-    mainWindow.loadURL(url.format({
-        pathname,
-        protocol: 'file:',
-        slashes: true,
-    }));
+    mainWindow.loadURL(
+        url.format({
+            pathname,
+            protocol: 'file:',
+            slashes: true,
+        })
+    );
+
+    const menu = Menu.buildFromTemplate([
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: 'Save',
+                    accelerator: 'CommandOrControl+S',
+                    click() {
+                        dialog.showSaveDialog(
+                            mainWindow,
+                            { defaultPath: 'Untitled.svg' },
+                            filename => {
+                                if (svg) {
+                                    fs.writeFile(filename, svg);
+                                }
+                            }
+                        );
+                    },
+                },
+                {
+                    label: 'Quit',
+                    accelerator: 'CommandOrControl+Q',
+                    click() {
+                        app.quit();
+                    },
+                },
+            ],
+        },
+        {
+            label: 'Help',
+            submenu: [
+                {
+                    label: 'About ' + app.getName(),
+                    click() {
+                        shell.openExternal(pkg.repository.url);
+                    },
+                },
+                {
+                    label: 'The DOT Language',
+                    click() {
+                        shell.openExternal('https://graphviz.gitlab.io/_pages/doc/info/lang.html');
+                    },
+                },
+                {
+                    label: 'Dot User Guide',
+                    click() {
+                        shell.openExternal('https://graphviz.gitlab.io/_pages/pdf/dotguide.pdf');
+                    },
+                },
+            ],
+        },
+    ]);
+
+    Menu.setApplicationMenu(menu);
 }
 
 app.on('ready', createWindow);
